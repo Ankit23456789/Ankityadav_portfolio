32a987a6ccb6efcc2f43d52afafedbbf7743a9d2
Ankit yadav <ankityadav281998@gmail.com>
2023-04-28 13:24:38 +0530

    homepage style changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,35 +3,35 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='container-fluid Home '>
-      <div className='container home-main'>
+    <div className='container Home '>
 
 
-        <div className='row '>
 
-          <div className='col-12 col-md-6 col-lg-6 heroimage-box'>
-            <img src="images/contactimage.JPG" alt="Ankit yadav" className='img-fluid hero-img' />
-          </div>
-          <div className=' col-12 col-md-6 col-lg-6 hero-textbox text-center'>
-            <h2>
-              Hello , my  name is Ankit yadav
-            </h2>
-            <h4> I'm  a Web Developer</h4>
+      <div className='row '>
 
-            <p className=' Home_textbox'> A web Developer with a passion for learning and creating.
-              This is my  offical portfolio website  to show's all details and work in web  development</p>
-            <div className='buttons container'>
-              <Link to={"./contactMe"}>  <button className='   Talk-button ' > Let's Connect</button></Link>
+        <div className='col-12 col-md-6 col-lg-6 heroimage-box'>
+          <img src="images/contactimage.JPG" alt="Ankit yadav" className='img-fluid hero-img' />
+        </div>
+        <div className=' col-12 col-md-6 col-lg-6 hero-textbox text-center'>
+          <h2>
+            Hello , my  name is Ankit yadav
+          </h2>
+          <h4> I'm  a Web Developer</h4>
+
+          <p className=' Home_textbox'> A web Developer with a passion for learning and creating.
+            This is my  offical portfolio website  to show's all details and work in web  development</p>
+          <div className='buttons container'>
+            <Link to={"./contactMe"}>  <button className='   Talk-button ' > Let's Connect</button></Link>
 
 
-            </div>
           </div>
         </div>
-
-
       </div>
 
+
     </div>
+
+
 
   )
 }
